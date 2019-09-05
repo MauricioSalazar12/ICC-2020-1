978b0c05c21b50d1ac8dcf4b68e5bc5cc8751b77
MauricioSalazar12 <54091623+MauricioSalazar12@users.noreply.github.com>
2019-09-05 11:49:27 -0500

    Add files via upload

--- practica06/Temperatura.java
+++ practica06/Temperatura.java
@@ -1,1 +1,50 @@
+public class Temperatura {
+       public static int CELCIUS = 0;
+       public static int FARENHEIT = 1;
+       
+       double valor;
+       int escala;
 
+       public Temperatura(double valor, int escala) {
+              this.valor = valor;
+              this.escala = escala; 
+                  }
+
+       public double convertirACelcius(double valorF){
+              return (valorF-32)/1.8;
+                  }
+
+       public double convertirAFarenheit(double valorC) {
+              return valorC*1.8+32;
+                  }
+
+       public double obtenerTemperaturaenCelcius(){
+                 if (escala == CELCIUS) {
+                      return valor;     
+                                        }
+                 else { 
+                       return convertirACelcius(valor);
+                      }
+                                                   }
+       public double obtenerTemperaturaenFarenheit(){
+                 if (escala == FARENHEIT) {
+                      return valor;
+                                          }
+                 else {
+                      return convertirAFarenheit(valor);
+                       }
+                                                    }
+
+       public boolean esIgual(Temperatura temp) {
+                 return escala == temp.escala && valor == temp.valor;
+                                                }
+
+       public boolean esMayor(Temperatura temp) {
+                 return obtenerTemperaturaenCelcius() > temp.obtenerTemperaturaenCelcius();
+              }
+       public boolean esMenor(Temperatura temp) {
+                 return obtenerTemperaturaenCelcius() < temp.obtenerTemperaturaenCelcius();
+              }
+           
+
+}
