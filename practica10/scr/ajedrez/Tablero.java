efed5c7738882795e190d6125ee4a4ab1880bdf4
MauricioSalazar12 <54091623+MauricioSalazar12@users.noreply.github.com>
2019-10-04 19:22:31 -0500

    Add files via upload

--- practica10/scr/ajedrez/Tablero.java
+++ practica10/scr/ajedrez/Tablero.java
@@ -1,1 +1,43 @@
+package ajedrez;
+import processing.core.PApplet;
 
+
+	public class Tablero extends PApplet {
+
+		public static void main(String[] args) {
+                    PApplet.main("ajedrez.Tablero");
+                }
+                @Override
+                public void settings() {
+                     size (800,800);
+                }
+		
+                @Override
+		public void setup() {
+                     
+		}
+		@Override
+		public void draw() {
+		     
+                     for (int j=0;j<700;j += 200) {
+                     	  
+			 
+				for (int i=0;i < 700;i += 200){ 
+					fill(0xFFFFFFFF);
+                     			rect(i,j,100,100);
+                                }
+                             
+		     }
+                     
+                     for (int j = 100;j < 800;j += 200){
+				
+				for (int i = 0;i < 700;i += 200){
+	             	                fill(0xFF000000);
+                     	                rect(i,j,100,100);
+                                    }
+		     }    
+                }
+
+
+     
+}
